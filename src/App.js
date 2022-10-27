e8b4f6c76805d8f2996bee7bca2b1b60f98bb7eb
Diogo Júnior <diogofbjr@gmail.com>
2022-10-27 18:31:10 -0300

    Casa menina mulher
    Vercel

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import 'react-toastify/dist/ReactToastify.css';
+import { BrowserRouter } from 'react-router-dom';
+import AuthProvider from './contexts/auth';
+import Routes from './routes';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // AuthProvider: é um componente funcional que contém o conteúdo que estava no App.tsx, e passamos o children, 
+    //ou seja, tudo que fica dentro desse provider será repassado no children
+    <AuthProvider>
+      <BrowserRouter>
+        <ToastContainer autoClose={3000} />
+        <Routes/>
+      </BrowserRouter>
+    </AuthProvider>
+    //BrowserRouter: é um componente responsável por informar a nossa aplicação que teremos um roteamento de componentes
   );
 }
 
