ea50432dac7da99c5aa47c02ef9bef03e40c713b
Diogo Júnior <diogofbjr@gmail.com>
2022-10-29 22:13:39 -0300

    atualização
    adicionando consulta

--- src/pages/Query/index.js
+++ src/pages/Query/index.js
@@ -5,7 +5,7 @@ import { useState, useEffect } from 'react';
 import Header from '../../components/Header';
 import Title from '../../components/Title';
 import { FiPlus, FiSearch, FiEdit2 } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+
 import { format } from 'date-fns';
 
 
