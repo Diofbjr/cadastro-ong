51af489dfd4067ffec2d73a2c51d3ba1324140b5
Diogo Júnior <diogofbjr@gmail.com>
2022-10-31 09:24:35 -0300

    atualização
    pagina inicial

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,11 +1,20 @@
 import Header from "../../components/Header";
+import Title from "../../components/Title";
+
+import { FiHome } from "react-icons/fi";
 
 export default function Dashboard(){
     return(
         <div>
             <Header/>
-            <div>
-            
+
+            <div className="content">
+                <Title name="Página Inicial">
+                <FiHome size={25} />
+                </Title>
+                <div className="container">
+
+                </div>
             </div>
         </div>
     )
